b81899a6359208306e233c56ea7a55ad1959f591
Saptam <saptampro2003@gmail.com>
2022-10-25 14:56:38 +0530

    Added feirbase.

--- practice-proj/hfhe/src/environments/environment.prod.ts
+++ practice-proj/hfhe/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'nayasa-jewel',
+    appId: '1:907454421991:web:c6fad25403545799d07b42',
+    storageBucket: 'nayasa-jewel.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBNvsrJrEy8hdGbK0xqAE2Q_YxJucXXPmU',
+    authDomain: 'nayasa-jewel.firebaseapp.com',
+    messagingSenderId: '907454421991',
+    measurementId: 'G-GV3YM09QQ8',
+  },
   production: true
 };
--- practice-proj/hfhe/src/environments/environment.ts
+++ practice-proj/hfhe/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'nayasa-jewel',
+    appId: '1:907454421991:web:c6fad25403545799d07b42',
+    storageBucket: 'nayasa-jewel.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBNvsrJrEy8hdGbK0xqAE2Q_YxJucXXPmU',
+    authDomain: 'nayasa-jewel.firebaseapp.com',
+    messagingSenderId: '907454421991',
+    measurementId: 'G-GV3YM09QQ8',
+  },
   production: false
 };
 
